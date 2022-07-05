7f4371548bad2ce8c055df4be902012f979f6856
skurtsov <skurtsov@gmail.com>
2022-07-05 12:35:27 +0200

    First commit

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Header = () =>{
+    return(
+    <h1>Header is here</h1>
+    );
+  }
+
+  export default Header;
---
+++ src/components/Content.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Content =() =>{
+    return(
+        <div className='content'>
+        <div class="obloj"><img src="https://i.pinimg.com/736x/98/b9/52/98b952001792e2b836669abf4d853712.jpg" alt=""/></div>
+    <div class="ava"><img src="https://fotografias.antena3.com/clipping/cmsimages01/2020/05/18/B82DD741-81C0-44E1-85BF-740D933A2763/58.jpg" alt=""/></div>
+    <div class="name-cont">Name+desc</div>
+    <div>My posts
+        <div>New post</div>
+        <div>Post1</div>
+        <div>Post2</div>
+        <div>Post3</div>
+    </div></div>
+    );
+}
+export default Content;
